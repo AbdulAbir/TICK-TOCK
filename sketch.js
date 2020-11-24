ed7b0f76e78d52c0b9a4a738da0f20d6dbf47756
Abdul Abir <71123123+AbdulAbir@users.noreply.github.com>
2020-11-24 15:07:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,51 +5,65 @@ var scAngle;
 
 function setup() {
   createCanvas(400,400);
-  hr = hour();
-  mn = minute();
-  sc = second();
+ 
+
+  angleMode(DEGREES);
 
 
 }
 
 function draw() {
   background(0); 
-  camera.position.x = 0;
-  camera.position.y = 0;
+  translate(200,200);
+    rotate(-90);
 
-  
+   hr = hour();
+  mn = minute();
+  sc = second();
  
-  angleMode(DEGREES);
+  
   scAngle = map(sc,0,60,0,360);
   mnAngle = map(mn,0,60,0,360);
-  hrAngle = map(hr,0,60,0,360);
+  hrAngle = map(hr%12,0,12,0,360);
 
   push();
-  translate(sc.x,sc.y);
+  
   rotate(scAngle);
   stroke(255,0,0);
   strokeWeight(7);
-  arc(100, 0, 50, 50, 0, PI);
+  
+ 
   line(0,0,100,0);
   pop();
 
    push();
-  translate(mn.x,mn.y);
+ 
   rotate(mnAngle);
   stroke(0,255,0);
   strokeWeight(7);
-  arc(110, 0, 50, 50, 0, PI);
+  
   line(0,0,70,0);
   pop();
 
    push();
-  translate(hr.x,hr.y);
+  
   rotate(hrAngle);
   stroke(0,0,250);
   strokeWeight(7);
-  arc(120, 0, 50, 50, 0, PI);
+ 
   line(0,0,50,0);
   pop();
 
+  strokeWeight(10);
+  noFill();
+  //Seconds
+  stroke(255,0,0);
+  arc(0,0,300,300,0,scAngle);
+  //Minutes
+  stroke(0,255,0);
+  arc(0,0,280,280,0,mnAngle);
+  //Hour
+  stroke(0,0,255);
+  arc(0,0,260,260,0,hrAngle);
   drawSprites();
 }
